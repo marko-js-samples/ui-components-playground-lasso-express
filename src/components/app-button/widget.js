a453b1b4dfe4804503d43f79c458ff40fe95a80e
Patrick Steele-Idem <pnidem@gmail.com>
2015-02-06 16:53:46 -0700

    use w-onclick

--- src/components/app-button/widget.js
+++ src/components/app-button/widget.js
@@ -1,20 +1,16 @@
 function Widget() {
-    var _this = this;
-
-    // NOTE: this.$() is short-hand for $(this.el)
-    this.$().click(function(e) {
-        // Every Widget instance is also an EventEmitter instance.
-        // We will emit a custom "click" event when a DOM click event
-        // is triggered
-        _this.emit('click', {
-            event: e // Pass along the DOM event in case it is helpful to others
-        });
-    });
-
-
 }
 
 Widget.prototype = {
+    handleClick: function(event) {
+        // Every Widget instance is also an EventEmitter instance.
+        // We will emit a custom "click" event when a DOM click event
+        // is triggered
+        this.emit('click', {
+            event: event // Pass along the DOM event in case it is helpful to others
+        });
+    },
+
     // Add any other methods here
     setVariant: function(variant) {
         // First remove all of the variant classes (this could be optimized)
