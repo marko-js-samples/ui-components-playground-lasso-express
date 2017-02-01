61cbde5386cf800328155bfe8ebd008a9523bdac
Patrick Steele-Idem <pnidem@gmail.com>
2017-02-01 11:06:40 -0700

    Upgraded to marko@4.0.0-rc.11

--- src/services/users-browser.js
+++ src/services/users-browser.js
@@ -1,7 +1,7 @@
 require('whatwg-fetch');
 
 exports.getUsers = function(options) {
-    return fetch(`/services/users?pageIndex=${options.pageIndex || 0}`)
+    return fetch('/services/users?pageIndex=' + (options.pageIndex || 0))
         .then(function(response) {
             return response.json();
         });
